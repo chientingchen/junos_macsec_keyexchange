12de69d4f4279c69322318e4243e621395725c1b
Junjie Wan <junjie@juniper.net>
2018-03-23 03:40:42 +0000

    Update KeyExchanger.py

--- KeyExchanger.py
+++ KeyExchanger.py
@@ -1,4 +1,5 @@
 import os
+from socket import *
 from jnpr.junos import Device
 from lxml import etree
 import argparse
@@ -26,6 +27,31 @@ def main():
     output = dev.cli('show lldp local-information', format='xml', warning=False)
 
     logger('Local management IP: ' + output.xpath('//lldp-local-management-address-address')[0].text)
+    
+    
+    remote_addr = output.xpath('//lldp-remote-management-address')[0].text
+    mgmt_addr = output.xpath('//lldp-local-management-address-address')[0].text
+    
+    HOST = mgmt_addr
+    PORT = 9999
+    
+    print 'starting:...'
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.bind((HOST, PORT))
+    with open(os.path.join('/var/tmp/','output_test.txt'), 'a') as f:
+        f.write('-----Connected-----\n')
+        data1, address = s.recvfrom(1024)
+        f.write(data1 + '\n')
+        s.sendto('I got ur 1st message', address)
+        data2, address = s.recvfrom(1024)
+        f.write(data2 + '\n')
+        s.sendto('I got ur 2nd message', address)
+        data3, address = s.recvfrom(1024)
+        f.write(data3 + '\n')
+        s.sendto('We cool, bye', address)
+        s.close()
+
+    
 
 def logger(strLog):
     with open(os.path.join('/var/tmp/','output.txt'), 'a') as target_config:
