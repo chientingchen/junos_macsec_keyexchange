513eb02394e51f67b7869e94c2a1a58511df1f61
“Jaxon <jaxonchen@juniper.net>
2018-06-07 11:52:54 +0800

    delete operation read configuration from file

--- delete_MACsec_interface.py
+++ delete_MACsec_interface.py
@@ -1,13 +1,31 @@
-import sys
-sys.path.insert(0, '/var/db/scripts/jet')
+import sys, os
+from yaml import load
+
+def logger(strLog):
+    with open(_INPUT_DATA['MACSEC']['LOG_PATH'], 'a') as target_config:
+        target_config.write(strLog+'\n')
+
+#Read Environment config
+THIS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),'../commit/')
+DATA_FILE = 'minion_environment.yaml'
+
+f=open(os.path.join(THIS_DIR,DATA_FILE))
+data=f.read()
+_INPUT_DATA=load(data)
+f.close()
+
+logger('Loading required library from ' + _INPUT_DATA['MACSEC']['INCLUDE_PATH'])
+sys.path.insert(0, _INPUT_DATA['MACSEC']['INCLUDE_PATH'])
+
 import requests, json
 import argparse
+
 
 arguments = {'ChassisID': 'Device chassis ID which delete macsec configuration resides in', 
              'Interface': 'Device interface which delete macsec configuration resides in'}
 
-SERVER_IP = '172.27.169.123'
-SERVER_PORT = '8080'
+SERVER_IP = _INPUT_DATA['Production']['SERVER_IP']
+SERVER_PORT = _INPUT_DATA['Production']['SERVER_PORT']
 
 
 def rest_request_put(ChassisID, Interface, URL_route):
@@ -40,10 +58,14 @@ def main():
     args = parser.parse_args()
 
     # Extract the value
-    print args.ChassisID
-    print args.Interface
+    print 'delete record with Chassis ID={0}, Interface={1}'.format(args.ChassisID,args.Interface)
+
+    logger('Sending delete request to server, Chassis ID={0}, Interface={1}'.format(args.ChassisID,args.Interface))
 
     rest_request_put(args.ChassisID, args.Interface, 'DeleteCAKCKN')
+
+    logger('Delete successful')
+
 
 if __name__ == '__main__':
     main()
