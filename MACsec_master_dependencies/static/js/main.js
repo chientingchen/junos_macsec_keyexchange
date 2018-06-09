5563b286725bc3fdeadc293a6e9ab8c0635882cb
“Jaxon <jaxonchen@juniper.net>
2018-06-09 17:58:19 +0800

    Get data from local

--- MACsec_master_dependencies/static/js/main.js
+++ MACsec_master_dependencies/static/js/main.js
@@ -10,7 +10,7 @@ distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRAN
 *******************>*/
 (function ($) {
     "use strict";
-    $.getJSON("http://172.27.169.153:8888/ListCAKCKN", function(data){
+    $.getJSON("/ListCAKCKN", function(data){
         for (var i = 0, len = data.length; i < len; i++) {
             console.log(data[i]);
     	    $(".table").append('<div class="row"> <div class="cell" data-title="Device A hostname">'+ data[i]['leaf_hostname']+'</div> <div class="cell" data-title="Device A interface">'+ data[i]['leaf_port'] + '</div> <div class="cell" data-title="Device B hostname">'+ data[i]['spine_hostname'] + '</div> <div class="cell" data-title="Device B interface">' + data[i]['spine_port'] + '</div><div class="cell" data-title="CKN">'+ data[i]['CKN'] + '</div><div class="cell" data-title="CAK">'+data[i].CAK+'</div></div>');
