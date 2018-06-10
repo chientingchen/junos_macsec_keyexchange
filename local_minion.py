3c952bd835f8466e778ca7fbdecbf94e9d36722f
“Jaxon <jaxonchen@juniper.net>
2018-06-10 12:01:24 +0800

    Enhance logging module

--- local_minion.py
+++ local_minion.py
@@ -20,16 +20,14 @@ from jnpr.junos import Device
 from lxml import etree
 from collections import namedtuple
 from yaml import load
+import logging
+import logging.handlers
 
 # Device checklist for macsec function support
 sep = '-'    # used for extract device info from device description.
 device_name = ''
 device_list = ['EX3400','EX3400-VC','EX4200','EX4200-VC','EX4300','EX4300-VC','EX4550','EX4550-VC','EX4600','EX4600-VC','EX9200','EX9200-VC','MX240','MX480','MX960','MX2008','MX2010','MX2020','MX10003','QFX5100','QFX5100-VC','QFX10008','QFX10016']
 device_list_license = ['EX4200','EX4200-VC','EX4300','EX4300-VC','EX4550','EX4550-VC','EX4600','EX4600-VC','EX9200','EX9200-VC','QFX5100','QFX5100-VC']
-
-def logger(strLog):
-    with open(_INPUT_DATA['MACSEC']['LOG_PATH'], 'a') as target_config:
-        target_config.write(strLog+'\n')
 
 #Read Environment config
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -40,14 +38,28 @@ data=f.read()
 _INPUT_DATA=load(data)
 f.close()
 
-#sys.path.insert(0, '/var/db/scripts/jet')
-logger('Loading required library from ' + _INPUT_DATA['MACSEC']['INCLUDE_PATH'])
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), _INPUT_DATA['MACSEC']['INCLUDE_PATH']))
 
+#Starting to load dependencies
 import requests, json
 from jnpr.junos.utils.config import Config
 from datetime import datetime
 import jcs
+
+log_file = os.path.join(os.path.dirname(__file__), _INPUT_DATA['MACSEC']['LOG_PATH'])
+
+log_file_size = 1638400
+log_file_count = 5
+log_level = [logging.INFO, logging.DEBUG][_INPUT_DATA['MACSEC']['DEBUG']]
+
+logger = logging.getLogger('local_minion')
+logger.setLevel(log_level)
+handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=log_file_size, backupCount=log_file_count)
+handler.setFormatter(logging.Formatter('[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s','%m-%d %H:%M:%S'))
+logger.addHandler(handler)
+
+logger.info('Loading required library from ' + _INPUT_DATA['MACSEC']['INCLUDE_PATH'])
+
 
 tuple_CKN_CAK = namedtuple('tuple_CKN_CAK', ['ckn', 'cak'])
 tuple_Query_CKNCAK = namedtuple('tuple_Query_CKNCAK', ['LocalChassisID','LocalInt','LocalHostname','RemoteChassisID','RemoteInt','RemoteHostname'])
@@ -126,7 +138,7 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 
 def DeployConfig(dev, local_int, ckn, cak, conn_name = None):
 
-    logger('====> In DeployConfig')
+    logger.debug('====> In DeployConfig')
 
     if conn_name is None:
         conn_name = id_generator()
@@ -139,11 +151,11 @@ def DeployConfig(dev, local_int, ckn, cak, conn_name = None):
         cu.pdiff()
         cu.commit()
     
-    logger('<===== Out DeployConfig')
+    logger.debug('<===== Out DeployConfig')
 
 def DeployConfig_jcs(local_int, ckn, cak, conn_name = None):
 
-    logger('====> In DeployConfig_jcs')
+    logger.debug('====> In DeployConfig_jcs')
 
     if conn_name is None:
         conn_name = id_generator()
@@ -167,11 +179,11 @@ def DeployConfig_jcs(local_int, ckn, cak, conn_name = None):
                     </security>""".format(conn_name, ckn, cak, local_int)
     jcs.emit_change(change_xml, "change", "xml")
     
-    logger('<==== Out DeployConfig_jcs')
+    logger.debug('<==== Out DeployConfig_jcs')
 
 def rest_request_post(query):
 
-    logger('====> In rest_request_post')
+    logger.debug('====> In rest_request_post')
 
     response = None
 
@@ -197,7 +209,7 @@ def rest_request_post(query):
         sys.exit(-1)
         print str(e)
 
-    logger('<==== Out rest_request_post')
+    logger.debug('<==== Out rest_request_post')
 
     return response
 
@@ -212,9 +224,9 @@ class InfoCollector():
 
     def getMACsec_conn_key(self):
 
-        logger('====> In getMACsec_conn_key')
+        logger.debug('====> In getMACsec_conn_key')
 
-        logger('Collecting local macsec connectivity information')
+        logger.info('Collecting local macsec connectivity information')
 
         data = self.dev.rpc.get_config(
             filter_xml=etree.XML('''
@@ -237,17 +249,17 @@ class InfoCollector():
 
             self.dictConnCKNCAK[conn_name] = ckn_cak
 
-        logger('Collecting info succuessful.')
-        logger('<==== Out getMACsec_conn_key')
+        logger.info('Collecting info succuessful.')
+        logger.debug('<==== Out getMACsec_conn_key')
 
         #returning data collected to make this module easily to reuse.
         return self.dictConnCKNCAK 
 
     def getMACsec_interface_conn(self):
 
-        logger('====> In getMACsec_interface_conn')
+        logger.debug('====> In getMACsec_interface_conn')
 
-        logger('Collecting local macsec interface information')
+        logger.info('Collecting local macsec interface information')
 
         data = self.dev.rpc.get_config(
             filter_xml=etree.XML('''
@@ -265,28 +277,28 @@ class InfoCollector():
             #dict['xe-0/0/1'] = cal
             self.dictLocalIntConn[item.find('name').text] = item.find('connectivity-association').text
 
-        logger('<==== Out getMACsec_interface_conn')
+        logger.debug('<==== Out getMACsec_interface_conn')
 
         #returning data collected to make this module easily to reuse.
         return self.dictLocalIntConn
 
     def get_local_id_hostname(self):
 
-        logger('====> In get_local_id_hostname')
-        logger('Collecting local chassis id and hostname information')
+        logger.debug('====> In get_local_id_hostname')
+        logger.info('Collecting local chassis id and hostname information')
 
         data = self.dev.rpc.get_lldp_local_info()        
         Local_ChassisID = data.find('lldp-local-chassis-id').text
         Local_Hostname = data.find('lldp-local-system-name').text
 
-        logger('<==== Out get_local_id_hostname')
+        logger.debug('<==== Out get_local_id_hostname')
 
         return Local_ChassisID, Local_Hostname
 
     def get_remote_ID_port_by_LLDP(self, local_int):
 
-        logger('====> In get_remote_ID_port_by_LLDP')
-        logger('Collecting remoete chassis id and hostname information by LLDP')
+        logger.debug('====> In get_remote_ID_port_by_LLDP')
+        logger.info('Collecting remoete chassis id and hostname information by LLDP')
 
         #Try to get following info from lldp:
         #   neighbor hostname connected by local interface
@@ -305,7 +317,7 @@ class InfoCollector():
             remote_int = item.find('lldp-remote-port-description').text
             remote_hostname = item.find('lldp-remote-system-name').text
 
-        logger('<==== Out get_remote_ID_port_by_LLDP')
+        logger.debug('<==== Out get_remote_ID_port_by_LLDP')
 
         return remote_chassisID, remote_int, remote_hostname
 
@@ -319,31 +331,31 @@ def main():
     # check if device supports macsec function.
     chassis_hardware = dev.rpc.get_chassis_inventory()
     device_description = chassis_hardware.xpath(".//description")[0].text
-    logger("device_description is: " + device_description)
+    logger.info("device_description is: " + device_description)
 
     device_name = device_description.split(sep, 1)[0]
-    logger("device_name is: " + device_name)
+    logger.info("device_name is: " + device_name)
 
     if device_name in device_list:
-        logger("This device supports macsec function!")
+        logger.info("This device supports macsec function!")
     else:
-        logger("Sorry, this is not the device we want...stop checking now")
+        logger.info("This device is not supporting MACsec funtion for now. Process abort.")
         return
 
     if device_name in device_list_license:
-        logger(
+        logger.info(
             "This device also requests a licnese to be installed for macsec function,start checking required license now...")
 
         licenses = dev.rpc.get_license_summary_information()
 
         for ifd in licenses.getiterator("feature-summary"):
             if (ifd.find("name").text.strip() == 'macsec'):
-                logger("We get the license named: " + ifd.find("name").text.strip())
-                logger("Okay, we have installed the macsec license!")
+                logger.info("License name: " + ifd.find("name").text.strip())
+                logger.info("MACsec license has installed.")
                 break
             else:
-                print("Still searching for required macsec license...")
-                logger("Still searching for required macsec license...")
+                print("Searching for required macsec license...")
+                logger.info("Searching for required macsec license...")
 
     #collecting MACsec info
     dictLocalIntConn = info.getMACsec_interface_conn()
@@ -360,17 +372,17 @@ def main():
 
         lstQueryCKNCAK.append(query)
 
-    logger('Information ready, prepared to query from remote master')
+    logger.info('Information ready, prepared to query from remote master')
 
     #Query preshared key from server.
     for query in lstQueryCKNCAK:
         #Get responding ckn & cak
         dict_ServerResponse = json.loads(rest_request_post(query).text)
-        logger('Got response from remote master')
+        logger.info('Got response from remote master')
 
         #Check existing ckn & cak match or not, if there's any.
         if dictLocalIntConn[query.LocalInt] in dictConnCKNCAK:
-            logger('pre-shared key comparison')
+            logger.info('pre-shared key comparison')
 
             #Get current configured preshared key
             cur_CKNCAK = dictConnCKNCAK[dictLocalIntConn[query.LocalInt]]
@@ -383,34 +395,34 @@ def main():
                     and dict_ServerResponse['ckn'] != None and dict_ServerResponse['cak'] != None
                 ):
                 #ckn cak needs to be updated.
-                logger('pre-shared key needs update')
+                logger.info('pre-shared key needs update')
 
                 jcs.emit_warning("Get latest pre-shared key from server, update it.")
                 DeployConfig_jcs(query.LocalInt, dict_ServerResponse['ckn'], dict_ServerResponse['cak'], dictLocalIntConn[query.LocalInt])
 
-                logger('finish pre-shared key update')
+                logger.info('finish pre-shared key update')
             else:
-                logger('pre-shared key match, skip update.')
+                logger.info('pre-shared key match, skip update.')
                 #ckn & cak matched, do not reconfigured.
                 pass
 
         else:
             #There's not exising pre-shared key, deploy it.
-            logger('pre-shared key not existed, need to deploy a new one.')
+            logger.info('pre-shared key not existed, need to deploy a new one.')
 
             if dict_ServerResponse['ckn'] != None and dict_ServerResponse['cak'] != None:
                 jcs.emit_warning("Automatically generate pre-shared key and deploy it.")
                 DeployConfig_jcs(query.LocalInt, dict_ServerResponse['ckn'], dict_ServerResponse['cak'], dictLocalIntConn[query.LocalInt])
-                logger('pre-shared key deployed.')
+                logger.info('pre-shared key deployed.')
             else:
                 #display error msg since there's no existing record in Database.
                 #Possible scenario:
                 #1. User delete the record and macsec configuration accidentally 
                 #   -> LLDP is not working. -> Cannot recover from error state.
                 #   -> Inform user to delete both side's macsec configuration, and make sure LLDP is up&running, then try again.
-                logger('No match record in remote_master\'s database, please delete related records \
+                logger.error('No match record in remote_master\'s database, please delete related records \
                         and make sure LLDP is up and running between devices')
-                logger('e.g. junos@MX480> op delete_MACsec_interface.py <Device ChassisID> <Device interface name>')
+                logger.error('e.g. junos@MX480> op delete_MACsec_interface.py <Device ChassisID> <Device interface name>')
                 jcs.emit_error("There's not matched pre-shared key in database, please delete both side's macsec configuration and try again.")
 
         # Check if MKA works
@@ -418,13 +430,11 @@ def main():
         for ifd in mka_sessions.getiterator("mka-session-information"):
             if ifd.find("interface-name") == query.LocalInt:
                 if ifd.find("latest-sak-key-identifier").text.strip() == '000000000000000000000000/0':
-                    pass
-#                    logger_macsec.warn("Oops, MASsec function is not working")
-#                    logger_macsec.warn(
-#                        "This interface %s may not support MACsec function, please have a check: https://apps.juniper.net/feature-explorer/search.html#q=MACsec")
+                    logger.error("Oops, MASsec function is not working")
+                    logger.error("This interface %s may not support MACsec function, please have a check: https://apps.juniper.net/feature-explorer/search.html#q=MACsec")
                 else:
                     pass
-                    #logger_macsec("Great! MASsec is working now")
+                    logger.info("Great! MASsec is working now")
             else:
                 pass
 
